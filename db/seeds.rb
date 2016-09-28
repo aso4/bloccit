15d529aeac82bd30ee1a2c21d3f90f9dccc1d3bf
aso4 <albertyso@gmail.com>
2016-09-28 18:44:19 -0400

    assignment 17 rails seeding data: done

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,25 +10,29 @@ require 'random_data'
 
 # Create Posts
 50.times do
-# #1
-  Post.create!(
-# #2
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
-  )
+    # #1
+    Post.create!(
+        # #2
+        title:  RandomData.random_sentence,
+        body:   RandomData.random_paragraph
+    )
 end
 posts = Post.all
+
+Post.find_or_create_by(title: "This Is A Test Post", body: "Testing one two three")
 
 # Create Comments
 # #3
 100.times do
-  Comment.create!(
-# #4
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
+    Comment.create!(
+        # #4
+        post: posts.sample,
+        body: RandomData.random_paragraph
+    )
 end
 
-puts "Seed finished"
+Comment.find_or_create_by(post: Post.find_or_create_by(title: "This Is A Test Post"), body: "Body of the comment")
+
+puts 'Seed finished'
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
