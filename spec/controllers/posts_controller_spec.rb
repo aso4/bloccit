c7907196e908a30374e186e50fdc61d1d598b2e3
aso4 <albertyso@gmail.com>
2016-10-05 14:58:22 -0400

    checkpoint 18 rails introduction to crud: done

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
 
+  let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -9,25 +11,29 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
+  it "assigns [my_post] to @posts" do
+    get :index
+    expect(assigns(:posts)).to eq([my_post])
   end
+  
+  #  describe "GET show" do
+  #    it "returns http success" do
+  #      get :show
+  #      expect(response).to have_http_status(:success)
+  #    end
+  #  end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+  #  describe "GET edit" do
+  #    it "returns http success" do
+  #      get :edit
+  #      expect(response).to have_http_status(:success)
+  #    end
+  #  end
 end
